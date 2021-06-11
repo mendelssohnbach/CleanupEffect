f81189200a2e35e840d0f7d0cc452bc7c6768f22
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-12 06:58:31 +0900

    update: コンポーネント組み込み

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Greeting from './components/Greeting';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Greeting />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return (
+    <div>
+      <h1>Greeting Good Day!</h1>
+    </div>
+  );
+}
+
+export default Greeting;
