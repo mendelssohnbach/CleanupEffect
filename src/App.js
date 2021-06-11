a20319edb30cc75cdcb503351db5857f068f0a44
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-12 07:07:12 +0900

    update: スタイル設定

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Greeting from './components/Greeting';
+import Picture from './components/Picture';
 
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
       <Greeting />
+      <Picture />
     </div>
   );
 }
---
+++ src/components/Picture.jsx
@@ -0,0 +1,12 @@
+import './picture.css';
+
+function Picture() {
+  return (
+    <div>
+      <h2 className="favorite">私のお気に入りの写真</h2>
+      <img src="http://unsplash.it/400/270?gravity=east" alt="" />
+    </div>
+  );
+}
+
+export default Picture;
